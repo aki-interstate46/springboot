b34fe3c99741cfa01e8acef2be78a46ae7673462
yuu_akikawa <qwerty8520a@gmail.com>
2024-08-02 18:48:30 +0900

    以下の対応を実施 #13
    ・Controller共通処理の実装
    ・名称変更

--- front/src/main/java/com/front/spring/controller/AccountController.java
+++ front/src/main/java/com/front/spring/controller/AccountController.java
@@ -1,17 +1,14 @@
 package com.front.spring.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.front.form.Sample;
-import com.front.spring.service.SampleService;
 import com.frontcommon.spring.controller.BaseFrontControler;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -20,31 +17,17 @@ import jakarta.servlet.http.HttpServletRequest;
 @RequestMapping(value = "/account")
 public class AccountController extends BaseFrontControler {
 	
-	private SampleService service;
-	
-	private static final String TABLE_NAME = "account";
+	private static final String PROCESS_NAME = "account";
 	
 	@ModelAttribute
-	public Sample setUp(Model model) {
-		this.init(TABLE_NAME);
+	public Sample setUp(HttpServletRequest request) {
+		this.init(PROCESS_NAME);
 		return new Sample();
-	}
-	
-	@Autowired
-	public void setSampleService(SampleService service) {
-		this.service = service;
 	}
 	
 	@GetMapping
 	public String get(@Validated Sample form, BindingResult result, Model model, HttpServletRequest request) throws Exception {
-		return TABLE_NAME;
-	}
-	
-	@PostMapping
-	public String post(@Validated Sample form, BindingResult result, Model model, HttpServletRequest request) throws Exception {
-		service.setForm(form).setBindingResult(result).setModel(model).setRequest(request);
-		service.postProcessor();
-		return TABLE_NAME;
+		return PROCESS_NAME;
 	}
 	
 }
---
+++ web-common/src/main/java/com/webcommon/spring/handler/RequestInterceptor.java
@@ -0,0 +1,75 @@
+package com.webcommon.spring.handler;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Controller制御用Handler
+ * 
+ * @author Y.AKI
+ * @version 1.0.0
+ */
+public class RequestInterceptor implements HandlerInterceptor {
+	
+	private long startTime;
+	
+	/**
+	 * preHandle処理除外パス
+	 */
+	private static final String[] PRE_HANDLE_PROCESS_IGNORE_PATH = new String[] {
+	    "^CONTEXT_PATH/css/.*$",
+	    "^CONTEXT_PATH/images/.*$",
+	    "^CONTEXT_PATH/script/.*$",
+	};
+	
+	/**
+	 * 初期処理
+	 */
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		if (this.sessionProcessExecCheck(request)) {
+			this.startTime = System.currentTimeMillis();
+			LoggerFactory.getLogger(RequestInterceptor.class).info("## preHandle, path={} method={}", request.getRequestURL(), request.getMethod());
+		}
+		return true;
+	}
+	
+	/**
+	 * レンダリング前に処理を実行
+	 */
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+		if (this.sessionProcessExecCheck(request)) {
+			LoggerFactory.getLogger(RequestInterceptor.class).debug("## postHandle");
+		}
+	}
+	
+	/**
+	 * 終了処理
+	 */
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+		if (this.sessionProcessExecCheck(request)) {
+			LoggerFactory.getLogger(this.getClass()).info("## afterCompletion, time={}, path={}, method={}", System.currentTimeMillis() - startTime, request.getRequestURL(), request.getMethod());
+		}
+	}
+	
+	/**
+	 * Session関連処理の実行判定
+	 * 
+	 * @param requestURI
+	 * @return
+	 */
+	private boolean sessionProcessExecCheck(HttpServletRequest request) {
+		for (String path : PRE_HANDLE_PROCESS_IGNORE_PATH) {
+			if (request.getRequestURI().matches(path.replaceFirst("CONTEXT_PATH", request.getContextPath()))) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
